d2fc3880725dacefdc743ce9cde27b448d10ac11
CDog <carsonluu737@gmail.com>
2022-12-15 16:57:08 -0800

    Edit script.js for first plan

---
+++ script.js
@@ -0,0 +1,6 @@
+// Start game with overarching function startGame; takes argument rounds to run game x amount of times
+// Get computer's choice with function getComputerChoice; returns random choice from options array
+// Get user's choice with prompt userChoice
+// Check if user wins with function playRound; takes userSelection and compSelection as arguments and returns outcome of the game
+// Add points to corresponding score variable with givePoint function; takes outcome as argument and increases pt depending on value
+// Check to see if counter variable reached amount of rounds; if yes break and display score/winner, otherwise keep playing
