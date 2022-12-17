51fc5f426596d7f2426a607031b065f8b4843e88
CDog <carsonluu737@gmail.com>
2022-12-16 21:34:49 -0800

    Edit script.js for fixing finalResult display

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Start game with overarching function startGame; takes argument rounds to run game x amount of times
 // Get computer's choice with function getComputerChoice; returns random choice from options array
-let options = ['rock', 'paper', 'scissors']
+let options = ['rock', 'paper', 'scissors'];
+let counter = 0; let rounds = 3;
 
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3);
@@ -9,22 +10,23 @@ function getComputerChoice() {
 
 // Get user's choice with prompt userChoice
 
-let userChoice = prompt('Rock, Paper, or Scissors? ')
-
 // Check if user wins with function playRound; takes userSelection and compSelection as arguments and returns outcome of the game
 
 function playRound(userSelection, compSelection) {
     let outcome;
     if (userSelection === compSelection) {
+        console.log(`USER PICKED: ${userSelection}, COMPUTER PICKED: ${compSelection}; TIE!!!`);
         outcome = 'Tie';
     }
     else if (userSelection !== compSelection) {
         if ((userSelection === 'rock' && compSelection === 'paper') || 
         (userSelection === 'paper' && compSelection === 'scissors') || 
         (userSelection === 'scissors' && compSelection === 'rock')) {
+            console.log(`USER PICKED: ${userSelection}, COMPUTER PICKED: ${compSelection}; LOSS!!!`);
             outcome = 'Loss';
         }
         else {
+            console.log(`USER PICKED: ${userSelection}, COMPUTER PICKED: ${compSelection}; WIN!!!`);
             outcome = 'Win';
         }
     }
@@ -42,9 +44,37 @@ function givePoint(outcome) {
     else if (outcome === 'Loss') {
         compScore++;
     }
-    console.log(`Score: ${userScore}, ${compScore}`)
+    console.log(`Score: ${userScore}, ${compScore}`);
 }
 
-givePoint(playRound(userChoice, getComputerChoice()));
+let finalResult;
+
+while (counter < rounds) {
+    let userChoice = prompt('Rock, Paper, or Scissors? ');
+    if (userChoice === 'quit') {
+        finalResult = 'quit';
+        break;
+    }
+    else if (userChoice === null || userChoice === undefined || !options.includes(userChoice)) {
+        console.log("You're weird for that, choose something else");
+    }
+    else {
+        givePoint(playRound(userChoice, getComputerChoice()));
+        counter++;
+    }
+}
+
+if (finalResult !== 'quit') {
+    if (userScore > compScore) {
+        console.log('USER WINS!!!');
+    }
+    else if (compScore > userScore) {
+        console.log('COMPUTER WINS!!!');
+    }
+    else {
+        console.log('USER AND COMPUTER TIE!!!');
+    }
+}
+
 
 // Check to see if counter variable reached amount of rounds; if yes break and display score/winner, otherwise keep playing
