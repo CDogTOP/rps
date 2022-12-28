46037d9845b10ef7558efe977827ea017bdf2a76
CDog <carsonluu737@gmail.com>
2022-12-28 13:21:06 -0800

    Edit script.js to add clarifying comments to game functionality, polish game features

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // Start game with overarching function startGame; takes argument rounds to run game x amount of times
+
 function startGame(rounds) {
-    // Get computer's choice with function getComputerChoice; returns random choice from options array
+
     let options = ['rock', 'paper', 'scissors'];
     let counter = 0; let gameRounds = rounds;
     
+    // Get computer's choice with function getComputerChoice; returns random choice from options array
+
     function getComputerChoice() {
         let randomChoice = Math.floor(Math.random() * 3);
-        return options[randomChoice]
+        return options[randomChoice];
     }
-
-    // Get user's choice with prompt userChoice
 
     // Check if user wins with function playRound; takes userSelection and compSelection as arguments and returns outcome of the game
 
@@ -45,12 +46,17 @@ function startGame(rounds) {
         else if (outcome === 'Loss') {
             compScore++;
         }
-        console.log(`Score: ${userScore}, ${compScore}`);
+        console.log(`User Score: ${userScore}, Computer Score: ${compScore}`);
     }
 
     let finalResult;
 
+    // As long as the counter (starts at 0) is less than the number of rounds, the game is run; only on valid inputs will the counter be incremented 
+
     while (counter < gameRounds) {
+
+        // Get user's choice with prompt userChoice; will quit upon request, reask upon invalid input, and play upon valid value along with increment of counter
+
         let userChoice = prompt('Rock, Paper, or Scissors? ');
         if (userChoice === 'quit') {
             finalResult = 'quit';
@@ -65,6 +71,8 @@ function startGame(rounds) {
         }
     }
 
+    // If user did not quit the game midway through, conditional will display the winner after rounds have been played 
+
     if (finalResult !== 'quit') {
         if (userScore > compScore) {
             console.log('USER WINS!!!');
@@ -77,8 +85,7 @@ function startGame(rounds) {
         }
     }
 
-
-    // Check to see if counter variable reached amount of rounds; if yes break and display score/winner, otherwise keep playing
+    // When score and winner is displayed, prompt asks if user would like to play again
 
     let playAgain = prompt('Would you like to play again? Type Y or N: ');
     if (playAgain === null || playAgain.toUpperCase() !== 'Y') {
@@ -92,6 +99,9 @@ function startGame(rounds) {
 function welcomePlayer() {
     console.clear();
     while (true) {
+
+        // Prompt asks user how many rounds to play; loop runs until the user quits, reasks upon invalid value, and starts upon valid value with introduction console log
+
         let rounds = prompt("HELLO! Welcome to Rock, Paper, Scissors! How many rounds would you like to play?");
         if (rounds === 'quit') {
             break;
